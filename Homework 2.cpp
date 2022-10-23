5f99c29a083a163aa26767e5408c834346d64220
DESKTOP-IQVHD5N\Bakry Rehawi <bakry29bbb@gmail.com>
2022-10-23 13:46:56 +0300

    HOMEWORK

---
+++ Homework 2.cpp
@@ -0,0 +1,26 @@
+﻿// Homework 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	string Name = "Bakry Rihawi";
+	int Age = 24;
+	string City = "Aleppo";
+	string Country = "Syria";
+	int MonthlySalary = 500000;
+	char Gemder = 'M';
+	bool Married = false;
+   cout << "*******************************\n";
+   cout << "Name: " << Name << "\n";
+   cout << "Age: " << Age << " years\n";
+   cout << "City: " << City << "\n";
+   cout << "Country: " << Country << "\n";
+   cout << "MonthlySalary:" << MonthlySalary << "\n";
+   cout << "Yearlysalary:" <<MonthlySalary*12  << "\n";
+   cout << "Gemder: " << Gemder << "\n";
+   cout << "Married: " << Married << "\n";
+   cout << "*******************************\n";
+}
+
---
+++ Homework 3.cpp
@@ -0,0 +1,15 @@
+﻿// Homework 3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int Num1 = 10;
+	int Num2 = 12; int Num3 = 50;
+	int Total = 0;
+	Total = Num1 + Num2 + Num3;
+	cout << "Num1 + Num2 + Num3 = " << Total << "\n";
+	
+}
+
