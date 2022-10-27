ace6035f1eb6b839a4b20f492d1826604cf2a433
DESKTOP-IQVHD5N\Bakry Rehawi <bakry29bbb@gmail.com>
2022-10-27 13:56:02 +0300

    homewoek projects c++

---
+++ Homework 4.cpp
@@ -0,0 +1,44 @@
+﻿// Homework 4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string Name;
+    int Age;
+    string City;
+    string Country;
+    int MonthlySalary;
+    int YearlySalary;
+    char Gender;
+    bool Married;
+
+    cout << "Please enter your name:\t ";
+    cin >> Name;
+    cout << "Please enter your age:\t ";
+    cin >> Age;
+    cout << "Please enter your city:\t ";
+    cin >> City;
+    cout << "Please enter your country:\t ";
+    cin >> Country;
+    cout << "Please enter your monthlySalary:\t ";
+    cin >> MonthlySalary;
+    cout << "Please enter your gender:\t ";
+    cin >> Gender;
+    cout << "Please enter your married:\t ";
+    cin >> Married;
+
+    cout << "******************************\n";
+    cout << "Name: " << Name << "\n";
+    cout << "Age: " << Age << "\n"; 
+    cout << "City: " << City << "\n"; 
+    cout << "Country: " << Country << "\n";
+    cout << "MonthlySalary: " << MonthlySalary << "\n";
+    cout << "YearlySalary: " << MonthlySalary*12 << "\n";
+    cout << "Gender: " << Gender << "\n";
+    cout << "Married: " << Married << "\n";
+    cout << "******************************\n";
+
+    
+}
---
+++ Homework 5.cpp
@@ -0,0 +1,159 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   
+    /*
+
+   
+    // #7   إيجاد نصف الرقم 
+    float Number1 = 0;
+    cout << "please Enter the Number\t\t";
+    cin >> Number1;
+    cout << "Half the Number is: " << Number1 / 2 << endl;
+    
+    */
+
+
+
+    /*
+    
+    // #9   إيجاد مجموع الأرقام
+
+    float Number2, Number3, Number4;
+    cout << "Enter the first Number\t";
+    cin >> Number2;
+    cout << "Enter the second Number\t";
+    cin >> Number3;
+    cout << "Enter the Third Number\t";
+    cin >> Number4;
+    cout << "result = " << Number2 + Number3 + Number4;
+
+    */
+
+    /*
+    
+    // #10   إيجاد معدل ثلاثة علامات
+      
+    int Mark1, Mark2, Mark3;
+    cout << "Enter the first Mark\t";
+    cin >> Mark1;
+    cout << "Enter the second Mark\t";
+    cin >> Mark2;
+    cout << "Enter the Third Mark\t";
+    cin >> Mark3;
+    cout << "result = " << (Mark1 + Mark2 + Mark3)/3;
+
+    */
+
+    /*
+    
+    //  #14   تبديل مكان رقمين أثناء العرض 
+    int x, y, z;
+    cout << "Enter the tow Number\n";
+    cin >> x;
+    cin >> y;
+    z = x;
+    x = y; \
+    cout << endl; 
+    cout << x << endl;
+    cout << z << endl; 
+
+    */
+ 
+    /*
+     // #15   إيجاد مساحة المربع = الطول *العرض
+        
+    float Length, offer;
+    cout << "Enter the height \t";
+    cin >> Length;
+    cout << "Enter the offer \t";
+    cin >> offer;
+    cout << "Area = " << height * offer << endl; 
+
+    */
+    
+
+    /*
+     
+   // #17  إيجاد مساحة المثلث = نصف طول القاعدة *الارتفاع       
+    float base_length , height
+    cout << "Enter the base_length \t";
+    cin >> base_length;
+    cout << "Enter the height \t";
+    cin >> height;
+    cout << "Area = " << base_length * height << endl;
+
+    */
+
+    /*
+    
+    // #19     حساب مساحة الدائرة عن طريق طول قطر الدائرة
+    float diameter;
+    const float pi = 3.14;
+    cout << "Enter the diameter\t";
+    cin >> diameter;
+    cout << "Area= " << (pi / 4) * diameter * diameter << endl;
+
+    */
+
+    /*
+     
+     
+    //#20     حساب مساحة دائرة عن طريق مربع محيط بها ومعرفة طول ضلع المربع
+   
+    float A ;
+    const float pi = 3.14;
+    cout << "Enter the length of the side of the square \t";
+    cin >> A;
+    cout << "Area= " << (pi / 4) * A * A << endl;
+
+    */
+
+    /*
+    //#21  'L' حساب مساحة الدائرة عن طريق محيط الدائرة الذي طوله 
+     
+    int L;
+    const float pi = 3.14;
+    cout << "Enter the circumference of the circle \t";
+    cin >> L;
+    cout << "Area= " << (L* L)/(4*pi) << endl;
+    */
+
+/*
+     //#22   حساب مساحة الدائرة وحولها مثلث متساوي الساقين ومعلومة الأطوال 
+     float a, b;
+     const float pi = 3.14;
+     cin >> a;
+     cin >> b;
+     cout << endl; 
+     cout << "Area= " << ((b * b * pi) / 4) * ((2 * a - b) / (2 * a + b));
+*/
+
+
+//#31
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
